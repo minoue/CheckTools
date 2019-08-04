7860e1cb5f266a084015dec0e4ffae826cf9b00d
minoue <michitaka.inoue@icloud.com>
2019-08-04 22:51:12 +0100

    Change version number

--- meshChecker/src/pluginMain.cpp
+++ meshChecker/src/pluginMain.cpp
@@ -1,9 +1,11 @@
 #include "meshChecker.h"
 #include <maya/MFnPlugin.h>
 
+static const char* const VERSION = "1.2.0";
+
 MStatus initializePlugin(MObject mObj)
 {
-    MFnPlugin fnPlugin(mObj, "Michitaka Inoue", "1.1.5", "Any");
+    MFnPlugin fnPlugin(mObj, "Michitaka Inoue", VERSION, "Any");
     fnPlugin.registerCommand("checkMesh", MeshChecker::creator, MeshChecker::newSyntax);
     return MS::kSuccess;
 }
